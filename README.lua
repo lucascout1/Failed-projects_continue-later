a7713798548d4cc5595783c4e8713ed7500fdeb0
lucascout1 <145732625+lucascout1@users.noreply.github.com>
2023-09-24 22:38:39 -0300

    README.lua
    README

---
+++ README.lua
@@ -0,0 +1,12 @@
+-- # Failed-projects_continue-later
+-- Open source scripts and projects that have failed or will be continued later
+-- There is nothing that works here, if u want things that work check out the "Roblox-Open-Source" repository
+-- You can try making stuff work and tell me if you have any tips, might help a lot
+
+local RandomTable = {
+  math.random(1, 50)
+}
+
+return RandomTable
+
+print(RandomTable)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Failed-projects_continue-later
-Open source scripts and projects that have failed or will be continued later
